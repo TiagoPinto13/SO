cdca90bed1d60f1cb488943c0838095c0ef97a8a
Tiago Daniel da Silva Pinto <145665067+TiagoPinto13@users.noreply.github.com>
2023-10-12 18:20:17 +0100

    Add files via upload

---
+++ aula3/3/vector_vprod.c
@@ -0,0 +1,9 @@
+#include "vector.h"
+vector* vector_vprod(vector* v1, vector* v2) {
+    vector* result = vector_new(
+        v1->y * v2->z - v1->z * v2->y,
+        v1->z * v2->x - v1->x * v2->z,
+        v1->x * v2->y - v1->y * v2->x
+    );
+    return result;
+}
